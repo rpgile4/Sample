aa0082a840458c14d61e17c4790c8276dd833ad6
rlift <rlift@localhost>
2018-08-22 09:01:25 -0400

    test 4

--- sample/src/main/java/sample/SampleTest.java
+++ sample/src/main/java/sample/SampleTest.java
@@ -5,7 +5,8 @@ public class SampleTest {
 	public static void main(String[] args) {
 		System.out.println("Testing");
 		System.out.println("Testing2");
-		
+		System.out.println("Testing3");
+		System.out.println("Testing4");
 	}
 
 }
